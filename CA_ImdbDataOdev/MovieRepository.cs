5b7a0e4dfae92292a39de7908d7ad9d42a15906f
iremTuncer <111491254+iremTuncer@users.noreply.github.com>
2022-09-11 19:39:42 +0300

    first commit

---
+++ CA_ImdbDataOdev/MovieRepository.cs
@@ -0,0 +1,107 @@
+﻿using CA_ImdbDataOdev.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA_ImdbDataOdev
+{
+    internal class MovieRepository
+    {
+        IMDBDataContext db = new IMDBDataContext();
+        public List<Movie> MovieList()
+        {
+            return db.Movies.ToList();
+        }
+        public void Listele()
+        {
+            foreach (var movie in db.Movies.ToList())
+            {
+                Console.WriteLine(movie.Title);
+            }
+        }
+        public void YilaGoreListe()
+        {
+            int gelenYıl = Convert.ToInt32(Console.ReadLine());
+            var result = from m in db.Movies
+                         where m.Year > gelenYıl
+                         select new
+                         {
+                             m.Year,
+                             m.Title
+                         };
+            foreach (var item in result.ToList())
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public void PuanaGoreListe()
+        {
+            
+            int gelenPuan = Convert.ToInt32(Console.ReadLine());
+            var result2 = db.Movies.Where(x => x.Rating > gelenPuan).Select(x => new
+            {
+                x.Rating,
+                x.Title
+            }).ToList();
+
+            foreach (var item in result2)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public void KelimeArama()
+        {
+            
+            string gelenKelime = Console.ReadLine();
+            var result3 = db.Movies.Where(x => x.Description.Contains(gelenKelime)).Select(x => new
+            {
+                x.Title,
+                x.Description
+            }).ToList();
+            foreach (var item in result3)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public void TurArama()
+        {
+            string gelenTur = Console.ReadLine();
+            var result4 = from m in db.Movies
+                          join mg in db.MovieGenres on m.Id equals mg.MovieId
+                          join g in db.Genres on mg.GenreId equals g.Id
+                          select new
+                          {
+                              Tur = g.Name,
+                              Title = m.Title
+                          };
+            foreach (var item in result4.Where(x => x.Tur == gelenTur))
+            {
+                Console.WriteLine($"Film adı:{item.Title} Tür:{item.Title}");
+            }
+        }
+        public void Rastgele()
+        {
+            Random rnd = new Random();
+            int rastgele = rnd.Next(1, 1001);
+            foreach (Movie item in db.Movies.Where(x=>x.Id==rastgele))
+            {
+                Console.WriteLine($"Film adı:{item.Title}");
+            }
+        }
+        public void PuanaGoreRastgeleFilm()
+        {
+            Random rnd2 = new Random();
+            int rastgele2 = rnd2.Next(1,101);
+            Console.WriteLine("Puan giriniz(1 ile 100 arasında):");
+            int puan = Convert.ToInt32(Console.ReadLine());
+            foreach(Movie item in db.Movies.Where(x=>x.Id == rastgele2 && x.Rating>puan))
+            {
+                Console.WriteLine($"Film adı:{item.Title} Puan:{item.Rating}");
+            }
+        }
+
+
+    }
+}
--- CA_ImdbDataOdev/Program.cs
+++ CA_ImdbDataOdev/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using CA_ImdbDataOdev.Models;
+using System.Collections.Generic;
 
 namespace CA_ImdbDataOdev
 {
@@ -8,71 +10,61 @@ namespace CA_ImdbDataOdev
     {
         static void Main(string[] args)
         {
-            IMDBDataContext db = new IMDBDataContext();
+            MovieRepository moviesrepository = new MovieRepository();
+            while(true)
+            {
+                Console.WriteLine($"1-Film Listeleme \n2-Seçilen yıla göre listeleme \n3-Seçilen puana göre listeleme \n4-Seçilen kelimeye göre listeleme \n5-Seçilen türe göre listeleme \n6-Rastgele film seçimi \n7-Puan aralığında rastgele film getirme ");
+                int gelen =int.Parse(Console.ReadLine());
 
-            foreach (var movie in db.Movies.ToList())
-            {
-                Console.WriteLine(movie.Title);
+                switch(gelen)
+                {
+                    case 1:
+                        moviesrepository.Listele();
+                        break;
+                    case 2:
+                        Console.WriteLine("Hangi yıldan sonraki filmleri listelemek istersiniz:");
+                        moviesrepository.YilaGoreListe();
+                        break;
+                    case 3:
+                        Console.WriteLine("Kaç puan ve üzeri filmleri listelemek istersiniz?");
+                        moviesrepository.PuanaGoreListe();
+                        break;
+                    case 4:
+                        Console.WriteLine("Hangi kelimeyi içeren filmleri listelemek istersiniz?");
+                        moviesrepository.KelimeArama();
+                        break;
+                    case 5:
+                        Console.WriteLine("Hangi tür filmleri listelemek istersiniz?");
+                        moviesrepository.TurArama();
+                        break;
+                    case 6:
+                        moviesrepository.Rastgele();
+                        break;
+                    case 7:
+                        moviesrepository.PuanaGoreRastgeleFilm();
+                        break;
+                    default:
+                        break;
+                }
             }
-            //Yıl değerinden sonra//
-            Console.WriteLine("Hangi yıldan sonraki filmleri listelemek istersiniz:");
-            int gelenYıl = Convert.ToInt32(Console.ReadLine());
-            var result = from m in db.Movies
-                         where m.Year > gelenYıl
-                         select new
-                         {
-                             m.Year,
-                             m.Title
-                         };
-            foreach (var item in result.ToList())
-            {
-                Console.WriteLine(item);
-            }
-            //Puan değerinden sonra//
-            Console.WriteLine("Kaç puan ve üzeri filmleri listelemek istersiniz?");
-            int gelenPuan = Convert.ToInt32(Console.ReadLine());
-            var result2 = db.Movies.Where(x => x.Rating > gelenPuan).Select(x => new
-            {
-                x.Rating,
-                x.Title
-            }).ToList();
 
-            foreach (var item in result2)
-            {
-                Console.WriteLine(item);
-            }
-            //Alınan değer açıklama bölümünde bulunan filmler//
-            Console.WriteLine("Hangi kelimeyi içeren filmleri listelemek istersiniz?");
-            string gelenKelime = Console.ReadLine();
-            var result3 = db.Movies.Where(x => x.Description.Contains(gelenKelime)).Select(x => new
-            {
-                x.Title,
-                x.Description
-            }).ToList();
-            foreach (var item in result3)
-            {
-                Console.WriteLine(item);
-            }
-            //Türe göre filmler//
-            Console.WriteLine("Hangi tür filmleri listelemek istersiniz?");
-            string gelenTur = Console.ReadLine();
-            var result4 = db.Genres.Where(x => x.Name.Contains(gelenKelime)).ToList();
-            foreach (var item in result4.ToList())
-            {
-                Console.WriteLine(item.Name);
-            }
-            //Rastgele film//
-            Console.WriteLine("Rastgele bir film getirmek için bir sayı girin:");
-            int gelenSayi = Convert.ToInt32(Console.ReadLine());
-            var result5 = db.Movies.Where(x=> x.Id == gelenSayi).Select(x => new 
-            {
-                x.Id, 
-                x.Title
-            }).ToList();
-            foreach (var item in result5)
-            {
-                Console.WriteLine(item);
-            }
+           
+
+
+
+
+
+
+            
+
+            
+            
+            
+            
+            
+           
+            
+            
 
            
 
